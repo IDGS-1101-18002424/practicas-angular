87d1d7b1d777ef318cf9b2a5c3452f78b2d9b7f8
IDGS-801-18002424 <18002424@alumnos.utleon.edu.mx>
2023-07-02 01:24:43 -0600

    ORRJ-02/07 Actividad terminada, se espera revision

--- src/app/pedido/pedido.component.ts
+++ src/app/pedido/pedido.component.ts
@@ -6,13 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./pedido.component.css']
 })
 export class PedidoComponent {
-  /*nombreCompleto: string;
-  direccion: string;
-  telefono: string;
-  fechaCompra: string;
-  tamanio: string;
-  ingredientes: string;
-  numeroPizzas: number;*/
   nombreCompleto: string = '';
   direccion: string = '';
   telefono: string = '';
@@ -33,50 +26,75 @@ export class PedidoComponent {
     this.ingredientes = '';
     this.numeroPizzas = 0;
   }
-  
+  precios: { [key: string]: number } = {
+    Chica: 40,
+    Mediana: 80,
+    Grande: 120
+  };
+  ingredientesDisponibles = [
+    { nombre: 'Jamón', seleccionado: false },
+    { nombre: 'Piña', seleccionado: false },
+    { nombre: 'Champiñones', seleccionado: false },
+    { nombre: 'Pepperoni', seleccionado: false },
+    { nombre: 'Salami', seleccionado: false },
+    
+  ];
+  pedidoEnviado: boolean = false;
+
+
   agregarPizza() {
-    const subtotal = this.calcularSubtotal(this.tamanio, this.numeroPizzas);
+    const ingredientesSeleccionados = this.ingredientesDisponibles
+      .filter(ingrediente => ingrediente.seleccionado)
+      .map(ingrediente => ingrediente.nombre);
+  
+    const costoIngredientes = ingredientesSeleccionados.length * 10;
+    const subtotal = this.calcularSubtotal(this.tamanio, this.numeroPizzas, costoIngredientes);
+    
     const pizza = {
+      nombreCompleto: this.nombreCompleto,
       tamanio: this.tamanio,
-      ingredientes: this.ingredientes,
+      ingredientes: ingredientesSeleccionados.join(', '),
       numeroPizzas: this.numeroPizzas,
       subtotal: subtotal
     };
+    
     this.detallePedido.push(pizza);
+    this.calcularCostoTotal();
   }
 
   quitarPizza(pizza: any) {
     const index = this.detallePedido.indexOf(pizza);
     if (index !== -1) {
       this.detallePedido.splice(index, 1);
+      this.calcularCostoTotal();
     }
   }
 
-  calcularSubtotal(tamanio: string, numeroPizzas: number): number {
-    // Lógica para calcular el subtotal de la pizza según el tamaño y el número de pizzas
-    // Puedes implementar tu propia lógica aquí
-    return 0;
+  calcularSubtotal(tamanio: string, numeroPizzas: number, costoIngredientes: number): number {
+    const precio = this.precios[tamanio];
+    const costoTotal = (precio + costoIngredientes) * numeroPizzas;
+    return costoTotal;
   }
-
+  
   finalizarPedido() {
+    this.calcularCostoTotal();
     this.mostrarConfirmacion = true;
-    this.costoTotal = this.calcularCostoTotal();
   }
 
   aceptarPedido() {
-    // Enviar el pedido al servidor o realizar las acciones necesarias
-    // para finalizar el pedido y redirigir a la página de confirmación
+    this.pedidoEnviado = true;
   }
+  
 
   editarPedido() {
     this.mostrarConfirmacion = false;
   }
 
-  calcularCostoTotal(): number {
+  calcularCostoTotal() {
     let total = 0;
     for (const pizza of this.detallePedido) {
       total += pizza.subtotal;
     }
-    return total;
+    this.costoTotal = total;
   }
 }
